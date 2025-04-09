52fec0f863850ede6175e21f6ed339e5eaf0eaf1
Chanri2025 <aniruddhag.appynitty@gmail.com>
2025-04-09 15:08:40 +0530

    Changing the port number

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def home():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 6000))
     app.run(host="0.0.0.0", port=port, debug=True)
