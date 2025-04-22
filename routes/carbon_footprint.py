16ea57c00be39b5bcc97de5a50cc091c04d1a8f1
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2025-04-22 13:35:36 +0530

    Database Upload Done

--- routes/carbon_footprint.py
+++ routes/carbon_footprint.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, request, jsonify
 from datetime import datetime
 import pytz
+from config.mongo_connection import db
 
 carbon_footprint_bp = Blueprint("carbon_footprint", __name__)
+carbon_credit_collection = db["carbon_footprint_collections"]
 
 
 @carbon_footprint_bp.route("/calculate", methods=["POST"])
 def calculate_carbon_footprint():
     try:
         data = request.json
+        reference_id = data.get("reference_id")
+        house_id = data.get("house_id")
         date_str = data.get("date")
         total_waste = float(data.get("total_waste", 0))
 
@@ -39,8 +43,10 @@ def calculate_carbon_footprint():
         ist = pytz.timezone('Asia/Kolkata')
         timestamp = datetime.now(ist).strftime("%d/%m/%Y %H:%M:%S")
 
-        return jsonify({
-            "message": "Carbon footprint calculated successfully",
+        # Prepare MongoDB document
+        doc = {
+            "reference_id": reference_id,
+            "house_id": house_id,
             "carbon_footprint": round(carbon_footprint, 2),
             "total_waste": round(total_waste, 2),
             "wet_waste": round(wet_waste, 2),
@@ -51,6 +57,18 @@ def calculate_carbon_footprint():
             "total_co2_eq": round(total_co2_eq, 2),
             "date": date_obj.strftime("%d/%m/%Y %H:%M"),
             "timestamp": timestamp
+        }
+
+        # Insert into MongoDB
+        carbon_credit_collection.insert_one(doc)
+
+        # Remove MongoDB _id field before returning JSON
+        doc.pop("_id", None)
+
+        # Return response
+        return jsonify({
+            "message": "Carbon footprint calculated successfully",
+            **doc
         }), 200
 
     except Exception as e:
